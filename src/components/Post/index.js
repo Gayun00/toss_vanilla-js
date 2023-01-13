6ed11eeb805581b4606534562eddea2114b2a3b2
=Gayun00 <gayunlee11@gmail.com>
2023-01-13 18:52:39 +0900

    Refactor: use innerHTML to render Post, Po stList
    - change Post render method to return template literal
    - change PostList render method to map Post and set innerHTML
    - change create method to render
    
    Related to: #2

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -8,35 +8,19 @@ export class Post {
     this.date = post.date;
   }
 
-  create() {
-    const $post = document.createElement("li");
-    const $thumbContainer = document.createElement("div");
-    const $textContainer = document.createElement("div");
-    const $thumbnail = document.createElement("img");
-    const $title = document.createElement("h2");
-    const $description = document.createElement("p");
-    const $date = document.createElement("p");
-
-    $post.appendChild($thumbContainer);
-    $post.appendChild($textContainer);
-
-    $thumbContainer.appendChild($thumbnail);
-    $thumbnail.setAttribute("src", this.thumbnail);
-
-    $textContainer.appendChild($title);
-    $textContainer.appendChild($description);
-    $textContainer.appendChild($date);
-
-    $title.innerText = this.title;
-    $description.innerText = this.description;
-    $date.innerText = this.date;
-
-    $post.classList.add("post_item");
-    $thumbnail.classList.add("post_thumbnail");
-    $title.classList.add("post_title");
-    $description.classList.add("post_description");
-    $date.classList.add("post_date");
-
-    return $post;
+  render() {
+    return `
+      <li class="post_item">
+        <div>
+          <img src=${this.thumbnail} class="post_thumbnail">
+        </div>
+        <div>
+          <h2 class="post_title">
+            ${this.title}
+          </h2>
+          <p class="post_description">${this.description}</p>
+          <p class="post_date">${this.date}</p>
+        </div>
+      </li>`;
   }
 }
