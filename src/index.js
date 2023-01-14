d897ec5845a92ce9d8c11d00742c79163e49e243
Gayun00 <gayunlee11@gmail.com>
2023-01-14 16:47:22 +0900

    Correct: element variables

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { ListPage } from "./pages/List.js";
 
 class App {
-  constructor(appRoot) {
-    this.listPage = new ListPage();
-    this.listPage.attachTo(appRoot);
+  constructor($appRoot) {
+    this.$listPage = new ListPage();
+    this.$listPage.attachTo($appRoot);
   }
 }
 
