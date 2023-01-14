350240dd9b4d635a2d679c14c111107c6516a856
Gayun00 <gayunlee11@gmail.com>
2023-01-14 23:25:45 +0900

    Feat: add PostList to list page
    - modify each component width
    - refactor PageComponent
    
    Related to: #2, #9

--- src/pages/List.js
+++ src/pages/List.js
@@ -2,16 +2,36 @@ import { PageComponent } from "../components/Page.js/index.js";
 import { Header } from "../components/Header.js/index.js";
 import { Banner } from "../components/Banner/index.js";
 import { Footer } from "../components/Footer/index.js";
+import { PostList } from "../components/PostList.js/index.js";
+
+import "./index.css";
 
 export class ListPage extends PageComponent {
   constructor() {
-    super(`<ul><ul>`);
+    super("list_page");
+
     const $header = new Header();
     const $banner = new Banner();
     const $footer = new Footer();
 
-    $header.attachTo(this.$page);
-    $banner.attachTo(this.$page);
-    $footer.attachTo(this.$page);
+    this.createList().then((data) => {
+      const $postList = new PostList(data);
+      $header.attachTo(this.$page);
+      $postList.attachTo(this.$page);
+      $banner.attachTo(this.$page);
+      $footer.attachTo(this.$page);
+    });
+  }
+
+  createList() {
+    return new Promise((resolve, reject) => {
+      fetch("http://localhost:5000/posts")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          resolve(data);
+        });
+    });
   }
 }
