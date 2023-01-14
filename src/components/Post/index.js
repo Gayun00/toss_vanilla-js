a2ee76e3fef3eecc947f342aea3592baf9e75cc0
Gayun00 <gayunlee11@gmail.com>
2023-01-15 01:35:03 +0900

    Feat: push history with each post id on click post
    - still in progress. need to catch historychange custom event when url changed after route.

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,17 +4,27 @@ import "./index.css";
 export class Post extends BaseComponent {
   constructor(post) {
     super(`
-    <li class="post_item">
-      <div>
+      <li class="post_item">
         <img src=${post.thumbnail} class="post_thumbnail">
-      </div>
-      <div>
-        <h2 class="post_title">
-          ${post.title}
-        </h2>
-        <p class="post_description">${post.description}</p>
-        <p class="post_date">${post.date}</p>
-      </div>
-    </li>`);
+        <div>
+          <h2 class="post_title">
+            ${post.title}
+          </h2>
+          <p class="post_description">${post.description}</p>
+          <p class="post_date">${post.date}</p>
+        </div>
+      </li>`);
+
+    window.addEventListener("historychange", ({ detail }) => {});
+
+    this.$element.addEventListener("click", () => {
+      const url = new URL(window.location);
+      window.history.pushState({}, "", `${url}detail/${post.id}`);
+
+      const historyChangeEvent = new CustomEvent("historychanged", {
+        detail: {},
+      });
+      dispatchEvent(historyChangeEvent);
+    });
   }
 }
