65493183612f1644825198df8c825b30174f6cc4
Gayun00 <gayunlee11@gmail.com>
2023-01-14 18:24:43 +0900

    Feat: add Banner component
    Related to: #12

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,3 +1,4 @@
+import { Banner } from "../components/Banner/index.js";
 import { Header } from "../components/Header.js/index.js";
 import { PageComponent } from "../components/Page.js/index.js";
 
@@ -5,6 +6,8 @@ export class ListPage extends PageComponent {
   constructor() {
     super(`<ul><ul>`);
     const $header = new Header();
+    const $banner = new Banner();
     $header.attachTo(this.$page);
+    $banner.attachTo(this.$page);
   }
 }
