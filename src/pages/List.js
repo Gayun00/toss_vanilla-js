e819192ae442ca9616abcb051ae9d563e9ed0131
Gayun00 <gayunlee11@gmail.com>
2023-01-14 19:19:32 +0900

    Feat: Footer component
    - add footer component
    - add global css variables
    - add reset.css
    - add footer to list page
    
    Related to: #14

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,13 +1,17 @@
-import { Banner } from "../components/Banner/index.js";
-import { Header } from "../components/Header.js/index.js";
 import { PageComponent } from "../components/Page.js/index.js";
+import { Header } from "../components/Header.js/index.js";
+import { Banner } from "../components/Banner/index.js";
+import { Footer } from "../components/Footer/index.js";
 
 export class ListPage extends PageComponent {
   constructor() {
     super(`<ul><ul>`);
     const $header = new Header();
     const $banner = new Banner();
+    const $footer = new Footer();
+
     $header.attachTo(this.$page);
     $banner.attachTo(this.$page);
+    $footer.attachTo(this.$page);
   }
 }
