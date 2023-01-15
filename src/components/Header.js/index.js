eecd8c6277903b868757167b6ef8ff1378a32ee0
Gayun00 <gayunlee11@gmail.com>
2023-01-15 18:17:46 +0900

    Style: list page
    - modify banner width to remove horizontal scroll
    - edit sementic tag: nav, article
    - fix position of nav
    - add hover effect on post
    - modify post list, nav max-width

--- src/components/Header.js/index.js
+++ src/components/Header.js/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 export class Header extends BaseComponent {
   constructor() {
     super(`
-      <header class="header">
+      <nav class="header">
         <div class="header_container">
-          <span class="logo">toss</span>
+          <span class="logo">toss tech</span>
           <span class="menu">
             <button class="menu_button">디자인</button>
             <button class="menu_button">개발</button>
             <button class="menu_button highlighted">채용 바로가기</button>
           </span>
           </div>
-      </header>
+      </nav>
     `);
   }
 }
