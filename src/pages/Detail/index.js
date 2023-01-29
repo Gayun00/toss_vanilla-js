e1663c0a7c012edee64c6a4c928eebf17f31af98
Gayun00 <gayunlee11@gmail.com>
2023-01-29 16:24:24 +0900

    Correct: detail page
    - delete id parameter in render method
    - call render method in init
    
    Related to: #35

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,28 +1,27 @@
+import { Router } from "../../router";
 import { PageComponent } from "../../components/PageComponent";
 import { PostTitle } from "../../components/PostTitle";
-import { Router } from "../../router";
 
 export class DetailPage extends PageComponent {
   constructor() {
     super("detail_page");
-    this.id = null;
     this.init();
   }
 
   init() {
     window.addEventListener("historychanged", () => {
       this.getParam();
+      this.render();
     });
   }
 
-  render(id) {
-    const $postTitle = new PostTitle(id).render();
+  render() {
+    const $postTitle = new PostTitle(this.id).render();
     this.add($postTitle);
   }
 
   getParam() {
     const { id } = Router.getInstance().getParam();
     this.id = id;
-    this.render(id);
   }
 }
