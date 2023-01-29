e24c0c0fb8805817edfb3a8a68c24f35362ede8a
gygy <67543454+Gayun00@users.noreply.github.com>
2023-01-29 19:48:59 +0900

    Refactor: use dependency injection to use routes in Router class (#36)
    * Refactor: use dependency injection to use routes in Router class
    
    - Router class should be able to
    be run without routes data
    - move routes data to outside of Router class
    
    * Feat: add redirection to post list page in default route path
    
    - navigate to "/post" path to render post list page when it's on "/" path
    - delete temporalily used renderPostListPage method
    
    Fixed: #30

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
+import { Router } from "./router";
 import { Base } from "./components/Base";
 import { ListPage } from "./pages/List";
-import { Router } from "./router";
+import { DetailPage } from "./pages/Detail";
 import "./css/reset.css";
 
 class App extends Base {
   constructor($app) {
     super($app);
-    this.$listPage = new ListPage().render();
-    this.add(this.$listPage);
     this.$app = $app;
+
+    this.routes = [
+      { path: "/post", page: new ListPage() },
+      { path: "/post/:id", page: new DetailPage() },
+    ];
 
     this.init();
   }
 
   init() {
     window.addEventListener("historychanged", this.renderPage);
+    window.addEventListener("popstate", this.renderPage);
+    window.addEventListener("load", this.redirectToPostListPage);
 
-    window.addEventListener("popstate", () => {
-      this.renderPage();
-    });
+    Router.getInstance().init(this.routes);
   }
 
   renderPage() {
     Router.getInstance().renderPage();
+  }
+
+  redirectToPostListPage() {
+    Router.getInstance().navigate("post");
   }
 }
 
