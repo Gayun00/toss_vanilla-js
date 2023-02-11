d47c6610b7565b17d3e1126b032f9971bbd0e3e2
Gayun00 <gayunlee11@gmail.com>
2023-02-12 01:41:37 +0900

    Chore: set up jest config to fix importing css issue

--- __mocks__/setupJest.js
+++ __mocks__/setupJest.js
@@ -1,7 +1,3 @@
-import { JSDOM } from "jsdom";
 import { TextEncoder, TextDecoder } from "util";
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-const url = "http://localhost";
-const documentHTML = '<!DOCTYPE html><div class="root"></div>';
-global.document = new JSDOM(documentHTML, { url });
