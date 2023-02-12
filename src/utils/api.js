a1bfb42fb2f8d986329cfa78e6786af45c8f2460
Gayun00 <gayunlee11@gmail.com>
2023-02-12 10:49:05 +0900

    Refactor: use fetchRequest as common fetch logic

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,17 @@
+import { API_SERVER } from "./constants";
+
+const fetchRequest = (url, method, param) => {
+  return fetch(
+    url,
+    param && {
+      method: method,
+      body: JSON.stringify(param),
+    }
+  );
+};
+
 export const getPosts = ({ onSuccess, onError }) =>
-  fetch("http://localhost:5000/posts")
+  fetchRequest(`${API_SERVER}/posts`)
     .then((res) => res.json())
     .then((data) => {
       onSuccess(data);
