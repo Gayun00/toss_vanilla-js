dd00b8631cc527a9a74a7668d75d0bef941beefc
Gayun00 <gayunlee11@gmail.com>
2023-02-16 21:33:54 +0900

    Chore: add jest ts config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs", "@babel/plugin-transform-runtime"],
+    },
+  },
 };
