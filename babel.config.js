f7dfe956a96f0661771776b67c52fbcba423a1ed
Gayun00 <gayunlee11@gmail.com>
2023-02-24 11:50:38 +0900

    Fix: jest setting after convert modules to ts

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
   env: {
     test: {
       plugins: ["@babel/plugin-transform-modules-commonjs", "@babel/plugin-transform-runtime"],
