fc408015fd7898e030a8966b011f42c7182c69d1
Gayun00 <gayunlee11@gmail.com>
2023-03-08 20:57:31 +0900

    Refactor: convert type to handle URLSearchParams parameter type

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,15 @@
 import { IArticles } from "./interfaces";
 import { API_SERVER } from "./constants";
-import { axios, AxiosOptions, Params } from "./axios";
+import { axios, AxiosOptions } from "./axios";
 
-export const getArticles = (handler: AxiosOptions<IArticles>) =>
-  axios.get<Params, IArticles>({
+interface GetArticlesParams {
+  size: number;
+  page: number;
+}
+
+export const getArticles = (params: GetArticlesParams, handler: AxiosOptions<IArticles>) =>
+  axios.get<GetArticlesParams, IArticles>({
     url: `${API_SERVER}/posts`,
     options: handler,
+    params,
   });
